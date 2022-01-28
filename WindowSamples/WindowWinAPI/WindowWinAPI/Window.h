a107ff4b0e056e858ed8956362bb082ed08277b5
dmhud <zzespoozz@gmail.com>
2022-01-29 02:37:09 +0300

    Window class

--- WindowSamples/WindowWinAPI/WindowWinAPI/Window.h
+++ WindowSamples/WindowWinAPI/WindowWinAPI/Window.h
@@ -1,6 +1,39 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <functional>
+
+#include <Windows.h>
+
+using SizeChangedCallback = std::function<void(uint32_t width, uint32_t height)>;
 
 class Window
 {
 public:
-    Window();
+    Window(uint32_t windowWidth, uint32_t windowHeight);
+    ~Window();
+
+    HWND HWnd()             const { return _hWnd; }
+    uint32_t WindowWidth()  const { return _windowWidth; }
+    uint32_t WindowHeight() const { return _windowHeight; }
+
+    void Show() const;
+    void SetWindowTitle(const std::wstring& str) const;
+
+    static bool HandleMessage(MSG& msg);
+
+public:
+    void SetSizeChangedCallback(SizeChangedCallback sizeChangedCallback);
+    void OnWindowSizeChanged(uint32_t width, uint32_t height);
+
+private:
+    HWND                _hWnd;
+    HINSTANCE           _hInstance;
+    const char*         _windowClassName;
+
+    uint32_t            _windowWidth;
+    uint32_t            _windowHeight;
+    SizeChangedCallback _sizeChangedCallback;
 };
+
--- WindowSamples/WindowWinAPI/WindowWinAPI/main.cpp
+++ WindowSamples/WindowWinAPI/WindowWinAPI/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+#include "Window.h"
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello WinAPI window!" << std::endl;
+
+    Window window(800, 600);
+    window.Show();
+
+    // Main message loop
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message)
+    {
+        if (window.HandleMessage(msg))
+        {
+            // Message handled by WinAPI
+        }
+    }
+
     return 0;
 }
